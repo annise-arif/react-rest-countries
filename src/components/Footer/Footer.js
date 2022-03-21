40e11e873ecef24d812e9a4972b886e8537a3abf
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-03-22 00:46:37 +0600

    my first React project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Copyright Protected MD: Arif</small></p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to my React website</h1>
+            <nav className='menu'>
+                <a href="/Home">Home</a>
+                <a href="/Shop">Shop</a>
+                <a href="/About">About us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
